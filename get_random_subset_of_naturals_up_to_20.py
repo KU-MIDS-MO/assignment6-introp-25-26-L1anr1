bb49342c0104edf8f90ae61ef6ffe157c3d844d1
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2025-12-27 21:39:53 +0200

    Update get_random_subset_of_naturals_up_to_20.py

--- get_random_subset_of_naturals_up_to_20.py
+++ get_random_subset_of_naturals_up_to_20.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 def get_random_subset_of_naturals_up_to_20():
-    """
-    Replace the code below with your own implementation.
-    """
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    mask = np.random.randint(0, 2**20)
+    
+    candidates = np.arange(1, 21)
+    
+    shifts = np.arange(20)
+    
+    bool_index = ((mask >> shifts) & 1).astype(bool)
 
-
+    return candidates[bool_index]
