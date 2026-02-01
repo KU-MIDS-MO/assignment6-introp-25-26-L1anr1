67d8429a3e8f6344103f6a3cce0574e097b138ef
L1anr1 <124812538+L1anr1@users.noreply.github.com>
2026-02-01 02:51:56 +0200

    Update get_random_subset_of_naturals_up_to_20.py
    simplified solution

--- get_random_subset_of_naturals_up_to_20.py
+++ get_random_subset_of_naturals_up_to_20.py
@@ -1,12 +1,10 @@
 import numpy as np
 
 def get_random_subset_of_naturals_up_to_20():
-    mask = np.random.randint(0, 2**20)
-    
     candidates = np.arange(1, 21)
     
-    shifts = np.arange(20)
+    mask = np.random.randint(0, 2, size=20)
     
-    bool_index = ((mask >> shifts) & 1).astype(bool)
-
-    return candidates[bool_index]
+    bool_mask = mask.astype(bool)
+    
+    return candidates[bool_mask]
